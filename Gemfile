5101768f18110695b7df02b67a06d3d5973456c1
bdwain <bryandwain@gmail.com>
2014-09-14 00:56:48 -0500

    make it an api

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 ruby "2.1.2"
 
 gem 'rails', '4.1.0'
+gem 'rails-api'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery
+class ApplicationController < ActionController::API
+  #protect_from_forgery
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
